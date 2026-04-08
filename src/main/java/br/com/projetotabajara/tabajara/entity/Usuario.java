c47a02e511acd561c1149d2a548db6475ac13d4c
DaividXavier <daividxavier103@gmail.com>
2026-04-07 22:52:42 -0300

    07/04

---
+++ src/main/java/br/com/projetotabajara/tabajara/entity/Usuario.java
@@ -0,0 +1,37 @@
+package br.com.projetotabajara.tabajara.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO) 
+    private Integer idUsuario;
+
+    @Column(nullable = false, length = 100)
+    private String nomeUsuario;
+
+    @Column(nullable = false, length = 100)
+    private String emailUsuario;
+
+    @Column(nullable = false, length = 40)
+    private String loginUsuario;
+
+    // 🔥 CORREÇÃO AQUI
+    @Column(nullable = false, length = 100)
+    private String senhaUsuario;
+
+    private String role = "ROLE_USER";          
+
+    private String resetToken;
+
+    private LocalDateTime tokenExpiracao;
+}
