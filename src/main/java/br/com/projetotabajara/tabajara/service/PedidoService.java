124bd9b87e528363251b46f57720f620be730762
Daivid Xavier <DaividXavier@users.noreply.github.com>
2026-06-16 22:51:47 -0300

    Padroniza home e adiciona relatorio PDF

--- src/main/java/br/com/projetotabajara/tabajara/service/PedidoService.java
+++ src/main/java/br/com/projetotabajara/tabajara/service/PedidoService.java
@@ -1,6 +1,7 @@
 package br.com.projetotabajara.tabajara.service;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,28 +13,33 @@ import br.com.projetotabajara.tabajara.repository.PedidoRepository;
 import br.com.projetotabajara.tabajara.repository.ProdutoRepository;
 
 @Service
-
 public class PedidoService {
-@Autowired
-private PedidoRepository pedidoRepository;
-@Autowired
-private ProdutoRepository produtoRepository;
 
-//Salvar pedido
-public Pedido salvarPedido(Pedido pedido) {
-pedido.setDataPedido(LocalDate.now());
-    for (ItemPedido item : pedido.getItens()) {
-        Produto produto = produtoRepository.findById(item.getProduto()
-        .getIdProduto()).orElseThrow(() -> new RuntimeException("Produto não encontrado"));
+    @Autowired
+    private PedidoRepository pedidoRepository;
 
-        item.setProduto(produto);
-        item.setPreco(produto.getValorProduto());
-        item.atualizarSubtotal();
-        item.setPedido(pedido);
-        
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
+    // Salvar pedido
+    public Pedido salvarPedido(Pedido pedido) {
+        pedido.setDataPedido(LocalDate.now());
+
+        for (ItemPedido item : pedido.getItens()) {
+            Produto produto = produtoRepository.findById(item.getProduto().getIdProduto())
+                    .orElseThrow(() -> new RuntimeException("Produto nao encontrado"));
+
+            item.setProduto(produto);
+            item.setPreco(produto.getValorProduto());
+            item.atualizarSubtotal();
+            item.setPedido(pedido);
+        }
+
+        pedido.atualizarTotal();
+        return pedidoRepository.save(pedido);
     }
-    pedido.atualizarTotal();
-    return pedidoRepository.save(pedido);
-}
 
+    public List<Pedido> findAll() {
+        return pedidoRepository.findAll();
+    }
 }
